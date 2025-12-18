6662713f896d0d335f965266a41e7edd65777cd8
nestcc <nestncc@outlook.com>
2025-12-18 19:20:11 +0800

    course2 chapter7

---
+++ Course2/ch7/ZombitFeeding.sol
@@ -0,0 +1,14 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+import "./zombiefactory.sol";
+
+contract ZombieFeeding is ZombieFactory {
+
+  // Start here
+
+  function feedAndMultiply(uint _zombieId, uint _targetDna) public view {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    Zombie storage myZombie = zombies[_zombieId];
+  }
+
+}
