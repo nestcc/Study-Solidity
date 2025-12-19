016df47388acd81068c6c4aea5aafde0987caa50
nestcc <nestncc@outlook.com>
2025-12-19 17:15:39 +0800

    chapter 8

--- Course2/ch8/ZombitFeeding.sol
+++ Course2/ch8/ZombitFeeding.sol
@@ -4,11 +4,14 @@ import "./zombiefactory.sol";
 
 contract ZombieFeeding is ZombieFactory {
 
-  // Start here
-
-  function feedAndMultiply(uint _zombieId, uint _targetDna) public view {
+  function feedAndMultiply(uint _zombieId, uint _targetDna) public {
     require(msg.sender == zombieToOwner[_zombieId]);
     Zombie storage myZombie = zombies[_zombieId];
+
+    _targetDna = _targetDna % dnaModulus;
+    uint newDna = (myZombie.dna + _targetDna) / 2;
+    
+    _createZombie("NoName", newDna);
   }
 
 }
